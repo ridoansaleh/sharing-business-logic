f8bf0c65fe2510534d7428bb6717e4b86ac387ca
RSN <ridoansalehnasution98@gmail.com>
2020-05-28 22:04:42 +0700

    update docs and add children props (#2)

---
+++ src/render_props/children/Authentication.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+function Authentication(props) {
+  const [isLogin, setLogin] = useState(false);
+  const [isChecked, setCheck] = useState(false);
+
+  const callApiToCheckLoginStatus = () => {
+    setTimeout(() => {
+      setCheck(true);
+      setLogin(true);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    callApiToCheckLoginStatus();
+  }, []);
+
+  if (!isChecked) {
+    return <h2>Loading..</h2>;
+  }
+  return props.children(isLogin);
+}
+
+export default Authentication;
